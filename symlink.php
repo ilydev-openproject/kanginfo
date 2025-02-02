2681b7886bebd442737c46a8fd629e77059fc2ab
Ilyas Maazib <ilyasmaazib9@gmail.com>
2025-02-02 19:13:09 +0700

    first commit

--- symlink.php
+++ symlink.php
@@ -1,4 +1,5 @@
-$targetFolder = $_SERVER['DOCUMENT_ROOT'].'/storage/app/public';
-$linkFolder = $_SERVER['DOCUMENT_ROOT'].'/public/storage';
-symlink($targetFolder,$linkFolder);
+<?php
+$targetFolder = $_SERVER['DOCUMENT_ROOT'] . '/storage/app/public';
+$linkFolder = $_SERVER['DOCUMENT_ROOT'] . '/public/storage';
+symlink($targetFolder, $linkFolder);
 echo 'Symlink process successfully completed';
